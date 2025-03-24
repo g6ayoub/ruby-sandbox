5f8e53741ecd84b9a6b04d67814c5fd8441c3abf
George Ayoub <149275120+g6ayoub@users.noreply.github.com>
2025-03-24 17:29:52 +0000

    Added Minitest gem and added comments

--- howdy.rb
+++ howdy.rb
@@ -1,23 +1,41 @@
 require "./goodbye.rb"
 require "./name.rb"
 require "active_support/all"
+require "pry-byebug"
 
 greeting = "hey there"
 
 puts greeting
 
-people = Hash.new
-people[:name]= "chris"
-people[:age] = 23
-pp people.class
-pp people["chris"]
-pp people.keys
-# class User
-#   attr_accessor :name
-#   attr_accessor :age
 
-#   def initialize (name, age)
-#     self.name = name
+
+
+
+
+
+
+
+
+
+#PRACTICING HASH MANIPULATION
+
+# people = {:person =>{:name => "", :age => ""}}
+# people[:person][:name]= "chris"
+# people[:person][:age] = 23
+# byebug
+# pp people.class
+# pp people[:person]
+# pp people.keys
+
+
+#PRACTICING CREATING A CLASS
+
+# class User
+#   attr_reader :name
+#   attr_reader :age
+
+#   def initialize(name, age)
+#     @name = name
 #     @age = age
 #   end
   
@@ -27,7 +45,7 @@ pp people.keys
 
 # end
 
+
 # new_user = User.new
-# new_user.name = "Chris"
-# new_user.age = 23
+# new_user = ("Chris, 23")
 # new_user.greeting
